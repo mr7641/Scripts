ad3f288ab969cbe59a8bd709b43ae9d39a9a0e6e
mr7641 <105139022+mr7641@users.noreply.github.com>
2022-12-18 15:35:41 +0700

    Update autokali.sh

--- autokali.sh
+++ autokali.sh
@@ -4,7 +4,7 @@ echo "Kali 2022.4"
 echo "Unzip rockyou"
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
 
-echo "Install Sublime text
+echo "Install Sublime text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
