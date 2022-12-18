e29927316232142e8ccf3fa4ad22495cf14d9176
mr7641 <105139022+mr7641@users.noreply.github.com>
2022-12-18 16:06:04 +0700

    Update autokali.sh

--- autokali.sh
+++ autokali.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
-echo "Kali 2022.4"
-echo "Unzip rockyou"
+LGREEN = '\033[1;32m'
+
+echo -e "${LGREEN}Kali 2022.4"
+echo "${LGREEN}Unzip rockyou"
 if test -f "/usr/share/wordlists/rockyou.txt.gz"; then
   sudo gunzip /usr/share/wordlists/rockyou.txt.gz
 fi
 
-echo "Install Sublime text"
+echo "${LGREEN}Install Sublime text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt install sublime-text
+sudo apt install -y sublime-text
 
-echo "Prepare Metasploit"
+echo "${LGREEN}Prepare Metasploit"
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 sudo msfdb init
 sudo apt update
-sudo apt install metasploit-framework
+sudo apt install -y metasploit-framework
 
-echo "Install Rclone"
-sudo apt install rclone
+echo "${LGREEN}Install Rclone"
+sudo apt install -y rclone
 
-echo "Install Visual Studio Code"
+echo "${LGREEN}Install Visual Studio Code"
 wget -O /tmp/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-sudo apt install /tmp/vscode.deb
+sudo apt install -y /tmp/vscode.deb
 
-echo "Install golang"
-sudo apt install golang
+echo "${LGREEN}Install golang"
+sudo apt install -y golang
 
-echo "Install bloodhound"
-sudo apt install apt-transport-https
-sudo apt install neo4j
+echo "${LGREEN}Install bloodhound"
+sudo apt install -y apt-transport-https
+sudo apt install -y neo4j
 wget -O /tmp/bloodhound.zip 'https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-linux-x64.zip'
 sudo unzip /tmp/bloodhound.zip -d /opt
