aaecfc754b4ca6e3ab92b89e05a5d1059b438d07
mr7641 <105139022+mr7641@users.noreply.github.com>
2022-12-19 04:36:30 -0500

    Update autokali.sh

--- autokali.sh
+++ autokali.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# to run the script
+# sudo bash autokali.sh <dropbox token>
+
 # Reset
 Color_Off='\033[0m'       # Text Reset
  
@@ -14,33 +17,33 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
 echo -e "${Green}Kali 2022.4${Color_Off}"
-echo -e "${Green}Unzip rockyou${Color_Off}"
+echo -e "${Green}===Unzip rockyou wordlists===${Color_Off}"
 if test -f "/usr/share/wordlists/rockyou.txt.gz"; then
   gunzip /usr/share/wordlists/rockyou.txt.gz
 fi
 
-echo -e "${Green}Install Sublime text${Color_Off}"
+echo -e "${Green}===Install Sublime text===${Color_Off}"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install -y sublime-text
+apt -qq update
+apt -qq install -y sublime-text
 
-echo -e "${Green}Prepare Metasploit${Color_Off}"
+echo -e "${Green}===Prepare Metasploit===${Color_Off}"
 systemctl start postgresql
 systemctl enable postgresql
 msfdb init
-apt update
-apt install -y metasploit-framework
+apt -qq update
+apt -qq install -y metasploit-framework
 
-echo -e "${Green}Install Visual Studio Code${Color_Off}"
-wget -O /tmp/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-apt install -y /tmp/vscode.deb
+echo -e "${Green}===Install Visual Studio Code===${Color_Off}"
+wget -qO /tmp/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+apt -qq install -y /tmp/vscode.deb
 
-echo -e "${Green}Install golang${Color_Off}"
-apt install -y golang
+echo -e "${Green}===Install golang===${Color_Off}"
+apt -qq install -y golang
 
-echo -e "${Green}Install and setup samba${Color_Off}"
-apt install -y samba
+echo -e "${Green}===Install and setup samba===${Color_Off}"
+apt -qq install -y samba
 cp /etc/samba/smb.conf /etc/samba/smb.conf.old
 cat <<EOF > /etc/samba/smb.conf
 [data]
@@ -51,42 +54,56 @@ EOF
 (echo "kali"; echo "kali") | smbpasswd -s -a kali
 systemctl start smbd nmbd
 
-echo -e "${Green}Install Chisel${Color_Off}"
+echo -e "${Green}===Install Chisel===${Color_Off}"
 cd /opt; git clone https://github.com/jpillora/chisel.git
 cd /opt/chisel; go build; env GOOS=windows GOARCH=amd64 go build -o chisel.exe -ldflags "-s -w"
 
-echo -e "${Green}Clone go shellcode${Color_Off}"
-cd /opt; git clone https://github.com/Ne0nd0g/go-shellcode.git
+echo -e "${Green}===Clone go shellcode===${Color_Off}"
+cd /opt; git clone -q https://github.com/Ne0nd0g/go-shellcode.git
 
-echo -e "${Green}Clone dirsearcher${Color_Off}"
-cd /opt; git clone https://github.com/maurosoria/dirsearch.git
+echo -e "${Green}===Clone dirsearcher===${Color_Off}"
+cd /opt; git clone -q https://github.com/maurosoria/dirsearch.git
 
-echo -e "${Green}Install mono for compiling C# project${Color_Off}"
-apt install -y mono-complete
+echo -e "${Green}===Install mono for compiling C# project===${Color_Off}"
+apt -qq install -y mono-complete
 
 echo -e "${Green}Install bloodhound${Color_Off}"
-apt install -y apt-transport-https
-apt install -y neo4j
-wget -O /tmp/bloodhound.zip 'https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-linux-x64.zip'
-unzip /tmp/bloodhound.zip -d /opt
+apt -qq install -y apt-transport-https
+apt -qq install -y neo4j
+wget -qO /tmp/bloodhound.zip 'https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-linux-x64.zip'
+unzip -qq /tmp/bloodhound.zip -d /opt
 
-# Create Workspaces directory
-mkdir /home/kali/Workspaces
 # Install rclone
-echo -e "${Green}Install Rclone${Color_Off}"
-apt install -y rclone
-# Configure rclone
+echo -e "${Green}===Install rclone===${Color_Off}"
+apt -qq install -y rclone
+# Configure rclone for root
+mkdir -p /root/.config/rclone
 cat <<EOF > /root/.config/rclone/rclone.conf
 [dropbox]
 type = dropbox
 token = {"access_token":"$1","token_type":"bearer","expiry":"0001-01-01T00:00:00Z"}
 EOF
-# synn /var/www/html
+# configure rclone for user
+mkdir -p /home/kali/.config/rclone
+cat <<EOF > /home/kali/.config/rclone/rclone.conf
+[dropbox]
+type = dropbox
+token = {"access_token":"$1","token_type":"bearer","expiry":"0001-01-01T00:00:00Z"}
+EOF
+# Create Workspaces directory
+mkdir /home/kali/Workspaces
+# Create .mozilla directory
+mkdir /home/kali/.mozilla
+# sync /var/www/html
 rclone sync dropbox:/Kali/html /var/www/html
 # sync /home/kali/Workspaces
 rclone sync dropbox:/Kali/Workspaces /home/kali/Workspaces
+# sync firefox
+rclone sync dropbox:/Kali/mozilla /home/kali/.mozilla
 # change owner of Workspaces
 chown -R kali:kali /home/kali/Workspaces
+# change owner of .mozilla
+chown -R kali:kali /home/kali/.mozilla
 
 echo -e "${Yellow}Manual task"
 echo -e "${Yellow}Run command ${Green}sudo neo4j console ${Yellow}then enter neo4j:neo4j as username:password to setup Neo4j"
